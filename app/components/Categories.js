a6c6943921b819baee348f94b2da3abe5aac913f
Supreet0 <supreetp2004@gmail.com>
2024-07-09 16:00:23 +0530

    Layout

---
+++ app/components/Categories.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Categories(){
+    return (
+        <>
+        <div className="w-[90%] my-9 flex flex-col mx-auto relative 2xl:w-[1536px] font-serif">
+            <div className="flex justify-center items-center gap-4 mt-5 lg:mt-8"> 
+                <h1 className="text-3xl">Categories</h1>
+            </div>
+           
+        </div>
+        </>
+    );
+
+}
--- app/components/Hero.js
+++ app/components/Hero.js
@@ -3,17 +3,16 @@ import React from "react";
 export default function Hero() {
   return (
     <>
-    <div className="bg-white text-gray-900 font-serif mt-40 max-w-xl px-20 relative">
+    <div className="bg-white text-gray-900 font-serif mt-36 max-w-xl px-20 relative">
       <h1 className="text-5xl font-normal leading-normal">Find perfect plants for your home</h1>
-      <p className="text-gray-800 leading-normal"> Beautiful plants that encourage you to get creative.</p>
+      <p className="text-gray-800 leading-normal"> <span className="mr-40">Beautiful plants that encourage you</span><span> to get creative.</span></p>
       <div className="mt-10">
         <button className="bg-black py-3 px-8 text-white hover:bg-gray-900">SHOP NOW</button>
       </div>
     </div>
     <div className="flex justify-center mt-12">
-    <img src="/image.jpeg" className="xl:absolute bottom-0 right-20"/>
+    <img src="/image.jpeg" className="xl:absolute bottom-10 right-10 h-[460px] w-[450px]"/>
     </div>
-    
   </>
   );
 }
