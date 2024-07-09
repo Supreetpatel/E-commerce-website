cf0ec2c5b6bf819d463bf50e0bcfbddbc46a1d04
Supreet0 <supreetp2004@gmail.com>
2024-07-09 19:52:43 +0530

    Categories

--- app/components/Categories.js
+++ app/components/Categories.js
@@ -3,12 +3,28 @@ import React from "react";
 export default function Categories(){
     return (
         <>
-        <div className="w-[90%] my-9 flex flex-col mx-auto relative 2xl:w-[1536px] font-serif">
-            <div className="flex justify-center items-center gap-4 mt-5 lg:mt-8"> 
-                <h1 className="text-3xl">Categories</h1>
+          <div className="w-[90%] my-9 flex flex-col mx-auto relative 2xl:w-[1536px]">
+            <div className="flex justify-center items-center gap-4 mt-5 lg:mt-8">
+                <h1 className="text-3xl font-serif font-medium">Categories</h1>
             </div>
-           
-        </div>
+            <div className="min-[1285px]:flex flex-wrap mt-8 gap-6 justify-center">
+               <div className="flex flex-col p-6 w-72">
+                    <img src="Forest.jpeg" /><h3 className="mt-2 font-serif font-light text-gray-600">Jungle Plants</h3>
+                </div>
+                <div className="flex flex-col p-6 w-72">
+                    <img src="Outdoor.jpeg" /><h3 className="mt-2 font-serif font-light text-gray-600">Outdoor Plants</h3>
+                </div>
+                <div className="flex flex-col p-6 w-72">
+                    <img src="Bedroom.jpeg" /><h3 className="mt-2 font-serif font-light text-gray-600">Bedroom Plants</h3>
+                </div>
+                <div className="flex flex-col p-6 w-72">
+                    <img src="Indoor.jpeg"/><h3 className="mt-2 font-serif font-light text-gray-600">Indoor Plants</h3>
+                </div>
+            </div>
+            <div>
+                <h1 className="">Love and work are to people what water and sunshine to plants </h1>
+            </div>
+            </div>
         </>
     );
 
--- app/components/Hero.js
+++ app/components/Hero.js
@@ -5,7 +5,7 @@ export default function Hero() {
     <>
     <div className="bg-white text-gray-900 font-serif mt-36 max-w-xl px-20 relative">
       <h1 className="text-5xl font-normal leading-normal">Find perfect plants for your home</h1>
-      <p className="text-gray-800 leading-normal"> <span className="mr-40">Beautiful plants that encourage you</span><span> to get creative.</span></p>
+      <p className="text-gray-600 leading-normal"> <span className="mr-40">Beautiful plants that encourage you</span><span> to get creative.</span></p>
       <div className="mt-10">
         <button className="bg-black py-3 px-8 text-white hover:bg-gray-900">SHOP NOW</button>
       </div>
